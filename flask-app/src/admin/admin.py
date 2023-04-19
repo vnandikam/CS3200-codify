e498553d0e2291ec063228a1afe283c7981e00f3
jessicaapeng <jessicadpeng2014@gmail.com>
2023-04-18 23:32:10 -0400

    admin tests

--- flask-app/src/admin/admin.py
+++ flask-app/src/admin/admin.py
@@ -4,10 +4,10 @@ from src import db
 
 admin = Blueprint('admin', __name__)
 
-# # Get all the timesheet info from the database
+# Get all the timesheet info from the database for the timelog dashboard
 
 
-@admin.route('/timesheet_info')
+@admin.route('/timesheet_info', methods=['GET'])
 def get_timesheet_info():
     cursor = db.get_db().cursor()
     query = '''
@@ -34,3 +34,98 @@ def get_timesheet_info():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+
+
+# Get all the employee info from the database for the employee dashboard
+
+@admin.route('/employee_info', methods=['GET'])
+def get_employee_info():
+    cursor = db.get_db().cursor()
+    query = '''
+        SELECT e.employee_id AS id, ei.fname as fname, e.dep as depname, e.title as role
+        FROM Employee_Info AS ei
+        JOIN Employee AS e
+        ON e.employee_id = ei.employee_id
+    
+        '''
+
+    cursor.execute(query)
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+
+# Get all the payroll info from the database
+
+@admin.route('/payroll_info', methods=['GET'])
+def get_office_resources_info():
+    cursor = db.get_db().cursor()
+    query = '''
+        SELECT o.resource_name AS resource, o.issue_date as issue
+        FROM officeResources AS o
+       
+    
+        '''
+
+    cursor.execute(query)
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+
+# Get all the office resources info from the database
+
+@admin.route('/office_resources_info', methods=['GET'])
+def get_office_resources_info():
+    cursor = db.get_db().cursor()
+    query = '''
+        SELECT o.resource_name AS resource, o.issue_date as issue
+        FROM officeResources AS o
+       
+    
+        '''
+
+    cursor.execute(query)
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
